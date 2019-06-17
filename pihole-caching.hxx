586e2085993df46bf0c84baafdf9ad7a1b7188b3
Unknown <viperman1271@hotmail.com>
2019-06-16 20:12:40 -0400

    Adding support from cmake for version numbers and ignoring the generated file

---
+++ pihole-caching.hxx
@@ -0,0 +1,5 @@
+#pragma once
+
+// the configured options and settings for pihole-caching
+#define pihole-caching_VERSION_MAJOR @pihole-caching_VERSION_MAJOR@
+#define pihole-caching_VERSION_MINOR @pihole-caching_VERSION_MINOR@
